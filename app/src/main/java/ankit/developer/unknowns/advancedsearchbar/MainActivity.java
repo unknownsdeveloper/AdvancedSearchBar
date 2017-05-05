aa6b2107a00404f1930c827ce2d0ba4b1933b1fe
Ankit sharma <unknownsdeveloper@gmail.com>
2017-05-05 16:22:52 +0530

    hello

--- app/src/main/java/ankit/developer/unknowns/advancedsearchbar/MainActivity.java
+++ app/src/main/java/ankit/developer/unknowns/advancedsearchbar/MainActivity.java
@@ -1,9 +1,12 @@
 package ankit.developer.unknowns.advancedsearchbar;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -27,133 +30,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setTitle("Material Search");
-
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
-
-
-
         lstView = (ListView)findViewById(R.id.lstView);
-
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,lstSource);
-
         lstView.setAdapter(adapter);
-
-
-
-
-
         searchView = (MaterialSearchView)findViewById(R.id.search_view);
 
-
-
         searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-
             @Override
-
             public void onSearchViewShown() {
-
-
-
             }
-
-
 
             @Override
-
             public void onSearchViewClosed() {
-
-
-
                 //If closed Search View , lstView will return default
-
                 lstView = (ListView)findViewById(R.id.lstView);
-
                 ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,lstSource);
-
                 lstView.setAdapter(adapter);
-
-
-
             }
-
         });
-
-
 
         searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-
             @Override
-
             public boolean onQueryTextSubmit(String query) {
-
                 return false;
-
             }
-
-
-
             @Override
-
             public boolean onQueryTextChange(String newText) {
-
-                if(newText != null && !newText.isEmpty()){
-
+                if (newText != null && !newText.isEmpty()) {
                     List<String> lstFound = new ArrayList<String>();
-
-                    for(String item:lstSource){
-
-                        if(item.contains(newText))
-
+                    for (String item : lstSource) {
+                        if (item.contains(newText))
                             lstFound.add(item);
-
                     }
-
-
-
-                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,lstFound);
-
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, lstFound);
                     lstView.setAdapter(adapter);
-
-                }
-
-                else{
-
+                } else {
                     //if search text is null
-
                     //return default
-
-                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,lstSource);
-
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, lstSource);
                     lstView.setAdapter(adapter);
-
                 }
-
                 return true;
-
             }
-
-
-
         });
-
-
-
     }
     @Override
-
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.menu_item,menu);
-
         MenuItem item = menu.findItem(R.id.action_search);
-
         searchView.setMenuItem(item);
-
         return true;
-
     }
 
-
+    @Override
+    public void onBackPressed() {
+        if (searchView.isSearchOpen()) {
+            searchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
